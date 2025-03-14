f2859024f0236dcc6ff97f7670a620235906debf
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2025-03-14 23:55:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/data',(req,res)=>{
-   return res.send({
-    status:true,
-    error:false,
-    message:"success data"
-   })
+app.get('/data', (req, res) => {
+  return res.send({
+    status: true,
+
+    error: false,
+
+    message: 'success data',
+  });
 });
 
-
 let port = 4001;
-app.listen(port,()=>{
-    console.log(`The server is running ${port}`)
-})
+app.listen(port, () => {
+  console.log(`The server is running ${port}`);
+});
